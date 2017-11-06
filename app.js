859208eeedba1529af617c5629fbd54a7143ee63
Nattaya32 <nam_hug@hotmail.com>
2017-11-06 10:08:05 +0700

    หน้าโหลดแต่รูปยังไม่มา

--- app.js
+++ app.js
@@ -13,18 +13,29 @@ const mysolarController = require('./controllers/mysolar.controller')
 const zodiacController = require('./controllers/zodiac.controller')
 const stargroupController = require('./controllers/stargroup.controller')
 const starlController = require('./controllers/starl.controller')
-<<<<<<< HEAD
+const loadController = require('./controllers/load.controller')
 
-=======
->>>>>>> 30ff4c245e79a90e4ea4f9469573f433c5b14aee
+ 
 
-app.get('/', homeController.index)
+// app.get('/', homeController.index)
+// app.get('/our-solar-system', solarSystemController.index)
+// app.get('/our-star', starsController.index)
+// app.get('/our-mysolar', mysolarController.index)
+// app.get('/our-zodiac',zodiacController.index)
+// app.get('/our-stargroup',stargroupController.index)
+// app.get('/our-starline',starlController.index)
+// app.get('/our-load',loadController.index)
+
+
+app.get('/home', homeController.index)
 app.get('/our-solar-system', solarSystemController.index)
 app.get('/our-star', starsController.index)
 app.get('/our-mysolar', mysolarController.index)
 app.get('/our-zodiac',zodiacController.index)
 app.get('/our-stargroup',stargroupController.index)
 app.get('/our-starline',starlController.index)
+app.get('/',loadController.index)
+
 
 app.use('/assets', express.static(__dirname + '/static/assets', { maxAge: 86400000 }))
 app.use('/images', express.static(__dirname + '/static/images', { maxAge: 86400000 }))
