7d53fd113b59406a5c900524c060cb1a26de5154
qaswedxz <32535568+qaswedxz@users.noreply.github.com>
2017-11-07 12:47:58 +0700

    popup หน้า help นะ

--- app.js
+++ app.js
@@ -14,8 +14,7 @@ const zodiacController = require('./controllers/zodiac.controller')
 const stargroupController = require('./controllers/stargroup.controller')
 const starlController = require('./controllers/starl.controller')
 const loadController = require('./controllers/load.controller')
-
- 
+const helpController = require('./controllers/help.controller')
 
 // app.get('/', homeController.index)
 // app.get('/our-solar-system', solarSystemController.index)
@@ -35,7 +34,7 @@ app.get('/our-zodiac',zodiacController.index)
 app.get('/our-stargroup',stargroupController.index)
 app.get('/our-starline',starlController.index)
 app.get('/',loadController.index)
-
+app.get('/help',helpController.index)
 
 app.use('/assets', express.static(__dirname + '/static/assets', { maxAge: 86400000 }))
 app.use('/images', express.static(__dirname + '/static/images', { maxAge: 86400000 }))
